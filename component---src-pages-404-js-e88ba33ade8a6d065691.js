b7c72c43277427d98791cd279a0139e1b6f9f69b
bakuzan <imannoyed4days@hotmail.co.uk>
2022-09-19 19:44:52 +0100

    Updates

---
+++ component---src-pages-404-js-e88ba33ade8a6d065691.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhierophant_green=self.webpackChunkhierophant_green||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),a=n(1597),o=n(5129),r=n(3176);t.default=()=>l.createElement(o.Z,null,l.createElement(r.Z,{title:"Page Not found"}),l.createElement("h2",null,"Not Found"),l.createElement("p",null,"This is not the page you are looking for..."),l.createElement(a.Link,{to:"/"},"Return to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-e88ba33ade8a6d065691.js.map
